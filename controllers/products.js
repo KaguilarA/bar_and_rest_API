3ff5e6e243aafcd355b0698d7a864cf6880169b3
Kevin Aguilar <kevin.231@hotmail.com>
2024-11-30 06:54:33 -0600

    Seed code for product controller

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,4 @@ import ProductModel from '../models/products.js';
 
 export const createProduct = async (req, res) => { }
 
-export const getAllProducts = async (req, res) => {
-  try {
-    const products = await ProductModel.getAllProducts();
-
-    res.status(200).json(products);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-}
+export const getAllProducts = async (req, res) => { }
