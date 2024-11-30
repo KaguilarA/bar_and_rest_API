23e2573db337d18a74dfcda4b58280911d6d40b6
Kevin Aguilar <kevin.231@hotmail.com>
2024-11-30 06:27:57 -0600

    Impement BaseModel on ProductModel

--- models/products.js
+++ models/products.js
@@ -1,61 +1,13 @@
-import mysqlConnection from './../connections/db.js';
+import { BaseModel } from './../utils/baseModel.js';
 
-class Product {
-  id;
+export default class ProductModel extends BaseModel {
 
-  constructor(id = null, name, type, stock, price, imageUrl) {
-    if (id) this.id = id;
+  constructor(name, type, stock, price, imageUrl, id = null) {
+    super(id);
     this.name = name;
     this.type = type;
     this.stock = stock;
     this.price = price;
     this.imageUrl = imageUrl;
   }
-
-  async save() {
-    try {
-      
-    } catch (err) {
-      throw new Error('Error while saving product');
-    }
-  }
-
-  async update() {
-    try {
-      
-    } catch (err) {
-      throw new Error('Error while updating product');
-    }
-  }
 }
-
-class ProductModel {
-
-  async createProduct(name, type, stock, price, imageUrl) {
-    try {
-      const newProduct = new Product(null, name, type, stock, price, imageUrl);
-      const saved = newProduct.save();
-
-      return saved;
-    } catch (err) {
-      throw new Error('Error while creating product');
-      
-    }
-  }
-
-  async getAllProducts() {
-    try {
-      const [rows] = await mysqlConnection.query('CALL GetAllProducts()');
-
-      rows[0].forEach(({ id, name, type, stock, price, imageUrl }, i) => 
-        rows[0][i] = new Product(id, name, type, stock, price, imageUrl)
-      );
-
-      return rows[0];
-    } catch (err) {
-      throw new Error('Error while fetching products');
-    }
-  }
-}
-
-export default new ProductModel();
