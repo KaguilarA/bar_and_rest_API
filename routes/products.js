f207ab70059b49911d7ca9e9b12a14550a847037
Kevin Aguilar <kevin.231@hotmail.com>
2024-12-03 20:58:31 -0600

    update prodct routes

--- routes/products.js
+++ routes/products.js
@@ -12,20 +12,60 @@ import {
 
 const router = express.Router();
 
+/**
+ * @route GET /products
+ * @description Get all products
+ * @access Public
+ */
 router.get('/', getAllProducts);
 
+/**
+ * @route GET /products/:id
+ * @description Get a product by ID
+ * @access Public
+ */
 router.get('/:id', getProductById);
 
+/**
+ * @route POST /products
+ * @description Create a new product
+ * @access Public
+ */
 router.post('/', createProduct);
 
+/**
+ * @route POST /products/type
+ * @description Get products by type
+ * @access Public
+ */
 router.post('/type', getProductByType);
 
+/**
+ * @route PATCH /products/stock
+ * @description Update the stock of a product
+ * @access Public
+ */
 router.patch('/stock', updateProductStock);
 
+/**
+ * @route PATCH /products/:id
+ * @description Activate a product
+ * @access Public
+ */
 router.patch('/:id', activateProduct);
 
+/**
+ * @route PUT /products
+ * @description Update a product
+ * @access Public
+ */
 router.put('/', updateProduct);
 
+/**
+ * @route DELETE /products/:id
+ * @description Disable a product
+ * @access Public
+ */
 router.delete('/:id', disableProduct);
 
 export default router;
